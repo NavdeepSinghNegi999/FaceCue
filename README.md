3778d452ab88b7fe0b7bd7c43502eae867a6b9d0
NavdeepSinghNegi999 <navdeepsinghnegi999@gmail.com>
2024-10-02 13:09:44 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Emotion_Detection
+Developing a robust CNN model for the FER-2013 dataset by addressing imbalanced data.
---
+++ main.py
@@ -0,0 +1,66 @@
+import keras
+import streamlit as st
+import numpy as np
+import cv2
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
+
+
+# @st.cache(allow_output_mutation=True)
+# def load_my_model():
+#     model = keras.models.load_model("VGG16_model.h5")
+#     return model
+# model = load_my_model()
+# model = keras.models.load_model("VGG16_model.h5")
+
+
+
+
+def index_to_emotion(index):
+    emotion_labels = {'angry': 0, 'disgust': 1, 'fear': 2, 'happy': 3, 'neutral': 4, 'sad': 5, 'surprise': 6}
+    index_to_emotion = {index:emotion for emotion, index in emotion_labels.items()}
+    emotion = index_to_emotion[index]
+    return emotion
+
+
+def model_prediction(test_image):
+    
+    image = keras.preprocessing.image.load_img(test_image, target_size=(150,150))
+    img_to_arr = keras.preprocessing.image.img_to_array(image)
+    
+    img_arr = np.array([img_to_arr]) 
+    img_arr = img_arr/255.0
+    
+    prediction = 1 #model.predict(img_arr)
+    result_index = np.argmax(prediction)
+    
+    return result_index
+
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+
+try:
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
+
+
+
+#sidebar
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page", ["Image Emotion Prediction"])
+
+# Home Page
+if(app_mode == "Image Emotion Prediction"):
+    st.header("Emotion Detection")
+    
+    # Load image
+    test_image = st.file_uploader("Choose an Image")
+    
+    if(st.button("Show Image")):
+        st.image(test_image, use_column_width=True)
+    
+    if(st.button("Predict")):
+        st.write("Our Prediction")
+        result_index  = model_prediction(test_image)
+        result = index_to_emotion(result_index)
+        st.success(f"{result}")
+        
